3174300a0dd5ebe93633c26f72f22aaee8edfe11
Shaina <101617596+imonagain@users.noreply.github.com>
2022-06-11 22:45:31 -0700

    Created dist folder and files

--- src/index.jsx
+++ src/index.jsx
@@ -6,13 +6,13 @@ import './index.scss';
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
-    render() {
-        return (
-            <div className="my-flix">
-                <div>Good morning</div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="my-flix">
+        <div>Good morning</div>
+      </div>
+    );
+  }
 }
 
 // Finds the root of your app
