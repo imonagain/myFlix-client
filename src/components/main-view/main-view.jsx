898952040fc2c2334054702d76fea1d65746f992
Shaina <101617596+imonagain@users.noreply.github.com>
2022-06-16 14:34:08 -0700

    Completed exercise 3.4

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import axios from "axios";
 
+import "./main-view.scss";
+
+import { RegistrationView } from "../registration-view/registration-view";
+import { LoginView } from "../login-view/login-view";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -12,17 +16,46 @@ export class MainView extends React.Component {
     this.state = {
       movies: [],
       selectedMovie: null,
+      registered: null,
+      user: null,
     };
   }
 
+  // When a movie is clicked, this function is invoked & updates state of 'selectedMovie' property to that movie
   setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie,
     });
   }
 
+  //  When a user successfully logs in, this function updates 'user' property in state to that particular user
+  onLoggedIn(user) {
+    this.setState({
+      user,
+    });
+  }
+
+  onRegister(registered) {
+    this.setState({
+      registered,
+    });
+  }
+
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user, registered } = this.state;
+
+    if (registered) {
+      return <RegistrationView onRegister={(bool) => this.onRegister(bool)} />;
+    }
+
+    if (!user) {
+      return (
+        <LoginView
+          onLoggedIn={(user) => this.onLoggedIn(user)}
+          onRegister={(bool) => this.onRegister(bool)}
+        />
+      );
+    }
 
     if (movies.length === 0) return <div className="main-view" />;
 
@@ -61,10 +94,6 @@ export class MainView extends React.Component {
       .catch((error) => {
         console.log(error);
       });
-  }
-
-  setSelectedMovie(newSelectedMovie) {
-    // same code
   }
 }
 
---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+import "./registration-view.scss";
+
+//user registration form taking necessary user details
+export function RegistrationView(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password, email, birthday);
+    props.onRegister(false);
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Birthday:
+        <input
+          type="date"
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </form>
+  );
+}
